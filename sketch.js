5e388692450bacb5543ac62e65015cbfb16da7ae
Jeffrey Liu <35406352+Jeffelini@users.noreply.github.com>
2018-08-02 22:43:07 -0400

    restart

--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
 var s;
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(600,600);
   s = new Snake();
+
 }
 
 function draw() {
@@ -13,12 +14,12 @@ function draw() {
 
 function keyPressed() {
   if (keyCode === UP_ARROW) {
-    s.dir(0,-1);
-  } else if ((keyCode === DOWN_ARROW) {
-    s.dir(0,1);
-  } else if (keyCode === LEFT_ARROW) {
-    s.dir(-1,0);
+    s.dir(0, -1);
+  } else if (keyCode === DOWN_ARROW) {
+    s.dir(0, 1);
   } else if (keyCode === RIGHT_ARROW) {
-    s.dir(1,0);
+    s.dir(1, 0);
+  } else if (keyCode === LEFT_ARROW) {
+    s.dir(-1, 0);
   }
 }
--- snake.js
+++ snake.js
@@ -4,11 +4,6 @@ function Snake() {
   this.xspeed = 1;
   this.yspeed = 0;
 
-  this.dir = function(x, y) {
-    this.xspeed = x;
-    this.yspeed = y;
-  }
-
   this.update = function() {
     this.x = this.x + this.xspeed;
     this.y = this.y + this.yspeed;
@@ -16,7 +11,7 @@ function Snake() {
 
   this.show = function() {
     fill(255);
-    rect(this.x, this.y, 10, 10);
-  }
+    rect(this.x, this.y, 10,10);
 
+  }
 }
