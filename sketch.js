4f07de5b9e1e2a15b88d5cba506bc5efa2506849
Jeffrey Liu <35406352+Jeffelini@users.noreply.github.com>
2018-08-02 22:30:57 -0400

    restarted on object

--- sketch.js
+++ sketch.js
@@ -1,10 +1,8 @@
 var s;
-var scl = 20;
 
 function setup() {
   createCanvas(600, 600);
   s = new Snake();
-  frameRate(10);
 }
 
 function draw() {
@@ -15,12 +13,12 @@ function draw() {
 
 function keyPressed() {
   if (keyCode === UP_ARROW) {
-    s.dir(0, -1);
-  } else if (keyCode === DOWN_ARROW) {
-    s.dir(0, 1);
-  } else if (keyCode === RIGHT_ARROW) {
-    s.dir(1, 0);
-  } else {
+    s.dir(0,-1);
+  } else if ((keyCode === DOWN_ARROW) {
+    s.dir(0,1);
+  } else if (keyCode === LEFT_ARROW) {
     s.dir(-1,0);
+  } else if (keyCode === RIGHT_ARROW) {
+    s.dir(1,0);
   }
 }
--- snake.js
+++ snake.js
@@ -1,4 +1,3 @@
-
 function Snake() {
   this.x = 0;
   this.y = 0;
@@ -11,16 +10,13 @@ function Snake() {
   }
 
   this.update = function() {
-    this.x = this.x + this.xspeed*scl;
-    this.y = this.y + this.yspeed*scl;
-
-    this.x = constrain(this.x, 0, width-scl);
-    this.y = constrain(this.y, 0, height-scl);
+    this.x = this.x + this.xspeed;
+    this.y = this.y + this.yspeed;
   }
 
   this.show = function() {
     fill(255);
-    rect(this.x, this.y, scl, scl);
-
+    rect(this.x, this.y, 10, 10);
   }
+
 }
